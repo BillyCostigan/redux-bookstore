0a97ea48f703b9ebadf1ca90ba14586d16a13c42
Leszek Kłoczko <132784@student.uwm.edu.pl>
2017-07-27 10:57:56 +0200

    commit with react, sass, bootstrap, babel and without redux

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './styles/style.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './components/main';
@@ -6,7 +8,7 @@ class AppComponent extends React.Component {
     render() {
         return (
             <div>
-                <h1>OK</h1>
+                <h1 className="alert-danger">OK</h1>
                 <Main/>
             </div>
         )
